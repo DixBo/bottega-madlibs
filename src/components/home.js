aa7818d4e2586d44d3626d8135f5c626c5bbd62b
DixBo <budboden@yahoo.com>
2018-07-21 12:34:05 -0600

    built our first functional component

---
+++ src/components/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Header from './header'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        { Header('Bottega Madlibs', 'Fill out the field below and click the generate button to see the Mad Lib story.') }
+      </div>
+    );
+  }
+}
+
+
+export default Home;
