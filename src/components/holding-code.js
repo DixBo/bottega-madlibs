1052a4a3616a234d483e951191c432d3ad46baa8
DixBo <budboden@yahoo.com>
2018-07-22 20:17:09 -0600

    add more inputData and added a place to add code that is not in use

---
+++ src/components/holding-code.js
@@ -0,0 +1,5 @@
+{ Input('Color', this.state.color, this.handleInputChange, 'color') }
+{ Input('Plural Noun', this.state.pluralNoun, this.handleInputChange, 'pluralNoun') }
+{ Input('Adjective', this.state.adjectiveOne, this.handleInputChange, 'adjectiveOne') }
+{ Input('Celebrity', this.state.celebOne, this.handleInputChange, 'celebOne') }
+data.title, data.state, this.handleInputChange, data.name
